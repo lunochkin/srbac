bdc23e9e2fe908272b44f201959c6b6ac1d99123
Maksim Lunochkin <maxlun86@gmail.com>
2013-02-24 19:36:56 +0400

    fix

--- views/authitem/assign.php
+++ views/authitem/assign.php
@@ -1,64 +1,52 @@
 <?php
-/**
- * assign.php
- *
- * @author Spyros Soldatos <spyros@valor.gr>
- * @link http://code.google.com/p/srbac/
- */
 
-/**
- * The Assign tabview view
- *
- * @author Spyros Soldatos <spyros@valor.gr>
- * @package srbac.views.authitem
- * @since 1.0.0
- */
+$this->breadcrumbs = array(
+    'Управление правилами доступа'
+);
+
+
 ?>
-<?php $this->breadcrumbs = array(
-    'Srbac Assign'
-)
-?>
-<?php if($this->module->getMessage() != ""){ ?>
+<?php if ($this->module->getMessage() != "") { ?>
 <div id="srbacError">
-  <?php echo $this->module->getMessage();?>
+    <?php echo $this->module->getMessage();?>
 </div>
 <?php } ?>
-<?php if($this->module->getShowHeader()) {
-  $this->renderPartial($this->module->header);
+<?php if ($this->module->getShowHeader()) {
+    $this->renderPartial($this->module->header);
 }
 ?>
 <div>
-  <?php
+    <?php
 
-  $this->renderPartial("frontpage");
+    $this->renderPartial("frontpage");
 
-  $tabs = array(
-      'tab1'=>array(
-      'title'=>Helper::translate('srbac','Users'),
-      'view'=>'tabViews/roleToUser',
-      ),
-      'tab2'=>array(
-      'title'=>Helper::translate('srbac','Roles'),
-      'view'=>'tabViews/taskToRole',
-      ),
-      'tab3'=>array(
-      'title'=>Helper::translate('srbac','Tasks'),
-      'view'=>'tabViews/operationToTask',
-      ),
-  );
-  ?>
-  <div class="horTab">
-    <?php 
-    Helper::publishCss($this->module->css);
-    $this->widget('system.web.widgets.CTabView',
-        array('tabs'=>$tabs,
-        'viewData'=>array('model'=>$model,'userid'=>$userid,'message'=>$message,'data'=>$data),
-        'cssFile'=>$this->module->getCssUrl(),
-    ));
+    $tabs = array(
+        'tab1' => array(
+            'title' => Helper::translate('srbac', 'Users'),
+            'view' => 'tabViews/roleToUser',
+        ),
+        'tab2' => array(
+            'title' => Helper::translate('srbac', 'Roles'),
+            'view' => 'tabViews/taskToRole',
+        ),
+        'tab3' => array(
+            'title' => Helper::translate('srbac', 'Tasks'),
+            'view' => 'tabViews/operationToTask',
+        ),
+    );
     ?>
-  </div>
+    <div class="horTab">
+        <?php
+        Helper::publishCss($this->module->css);
+        $this->widget('system.web.widgets.CTabView',
+            array('tabs' => $tabs,
+                'viewData' => array('model' => $model, 'userid' => $userid, 'message' => $message, 'data' => $data),
+                'cssFile' => $this->module->getCssUrl(),
+            ));
+        ?>
+    </div>
 </div>
-<?php if($this->module->getShowFooter()) {
-  $this->renderPartial($this->module->footer);
+<?php if ($this->module->getShowFooter()) {
+    $this->renderPartial($this->module->footer);
 }
 ?>
