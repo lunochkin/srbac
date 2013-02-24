371ce055a11ca37658bbbcb6f4965382149d7464
Maksim Lunochkin <maxlun86@gmail.com>
2013-02-24 23:11:04 +0400

    fix

--- views/authitem/allowed.php
+++ views/authitem/allowed.php
@@ -48,14 +48,6 @@ $this->widget('system.web.widgets.CTabView',
 <?php echo SHtml::endForm(); ?>
 <!--Adjust tabview height--->
 <script type="text/javascript">
-    var tabsHeight = $(".tabs").height();
-    if (tabsHeight > 260) {
-        $(".view").height(tabsHeight - 16);
-    } else {
-        $(".view").height(260);
-        $(".tabs").attr("style", "border-bottom:none");
-
-    }
 
     $(".tabbable.tabs-left ul.tabs")
             .addClass("nav")
