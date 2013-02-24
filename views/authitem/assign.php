633b2e44112ce18da21360c758ff7272e5987f39
Maksim Lunochkin <maxlun86@gmail.com>
2013-02-24 23:38:58 +0400

    fix

--- views/authitem/assign.php
+++ views/authitem/assign.php
@@ -36,14 +36,14 @@ $this->breadcrumbs = array(
     );
     ?>
     <div id='tabsAssign'>
-    <?php
-    Helper::publishCss($this->module->css);
-    $this->widget('system.web.widgets.CTabView',
-        array('tabs' => $tabs,
-            'viewData' => array('model' => $model, 'userid' => $userid, 'message' => $message, 'data' => $data),
-            'cssFile' => $this->module->getCssUrl(),
-        ));
-    ?>
+        <?php
+        Helper::publishCss($this->module->css);
+        $this->widget('system.web.widgets.CTabView',
+            array('tabs' => $tabs,
+                'viewData' => array('model' => $model, 'userid' => $userid, 'message' => $message, 'data' => $data),
+                'cssFile' => $this->module->getCssUrl(),
+            ));
+        ?>
     </div>
 </div>
 <?php if ($this->module->getShowFooter()) {
@@ -52,22 +52,24 @@ $this->breadcrumbs = array(
 ?>
 
 <script type="text/javascript">
-    $("#tabsAssign ul")
-            .addClass("nav")
-            .addClass("nav-tabs");
-    $("#tabsAssign ul").find("a").attr("data-toggle", "tab");
-    $("#tabsAssign ul").removeClass("tabs").removeClass('yiiTab');
-    $("#tabsAssign div.view")
-            .addClass("tab-pane")
-            .removeClass("view")
-    ;
-    $("#tabsAssign div.tab-pane").wrapAll("<div class='tab-content' />");
+    $(document).ready(function () {
+        $("#tabsAssign ul")
+                .addClass("nav")
+                .addClass("nav-tabs");
+        $("#tabsAssign ul").find("a").attr("data-toggle", "tab");
+        $("#tabsAssign ul").removeClass("tabs").removeClass('yiiTab');
+        $("#tabsAssign div.view")
+                .addClass("tab-pane")
+                .removeClass("view")
+        ;
+        $("#tabsAssign div.tab-pane").wrapAll("<div class='tab-content' />");
 
-    $("#tabsAssign ul li a").on("click", function() {
+        $("#tabsAssign ul li a").on("click", function () {
 
-        $("#tabsAssign ul li").removeClass("active");
-        $(this).closest("li").addClass("active");
+            $("#tabsAssign ul li").removeClass("active");
+            $(this).closest("li").addClass("active");
+        });
+
+        $("#tabsAssign ul li:first a").trigger("click");
     });
-
-    $("#tabsAssign ul li:first a").trigger("click");
 </script>
